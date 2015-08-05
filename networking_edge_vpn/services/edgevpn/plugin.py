4ed7f97df020dadcffb68165bb5dfd47194c96b2
Pradeep Sathasivam <psathasi@brocade.com>
2015-08-05 16:56:10 +0530

    Edge VPN service with brocade service plugin
    Implements support for creation of Edge VPNs for inter datacenter
    connectivity.
    Added Edge VPN service and brocade implementation of the service
    as a plugin.
    Also Renamed directory from networking-edge-vpn to networking_edge_vpn.
    
    Change-Id: I2f8fdef0bc179fd6560d669cb7af9195354513fe

---
+++ networking_edge_vpn/services/edgevpn/plugin.py
@@ -0,0 +1,120 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+from networking_edge_vpn.db.edgevpn import edgevpn_db
+
+from neutron.i18n import _LI
+from neutron.plugins.common import constants
+from neutron.services import service_base
+from oslo_log import log as logging
+
+LOG = logging.getLogger(__name__)
+
+
+class EdgeVPNPlugin(edgevpn_db.EdgeVPNPluginDb):
+
+    """Implementation of the Edge VPN Service Plugin.
+
+    This class manages the work flow of EdgeVPN request/response.
+    Most DB related works are implemented in class
+    edgevpn_db.EdgeVPNPluginDb.
+    """
+
+    supported_extension_aliases = ["edgevpn", "service-type"]
+    path_prefix = "/edgevpn"
+
+
+class EdgeVPNDriverPlugin(EdgeVPNPlugin):
+
+    def __init__(self):
+        super(EdgeVPNDriverPlugin, self).__init__()
+        # Load the service driver from neutron.conf.
+        drivers, default_provider = service_base.load_drivers(
+            constants.EDGEVPN, self)
+        LOG.info(_LI("EdgeVPN plugin service driver: %s"), default_provider)
+        self.edgevpn_driver = drivers[default_provider]
+
+    def _get_driver(self):
+        return self.edgevpn_driver
+
+    def create_edgevpn(self, context, edgevpn):
+        """Call super to create row in DB."""
+        edgevpn = super(EdgeVPNDriverPlugin, self).create_edgevpn(context,
+                                                                  edgevpn)
+        """Get service driver."""
+        driver = self._get_driver()
+        """Call service driver to create edgevpn instance on device."""
+        edgevpn = driver.create_edgevpn(context, edgevpn)
+        (super(EdgeVPNDriverPlugin, self).
+            update_edgevpn_status_and_name(context, edgevpn))
+        return edgevpn
+
+    def update_edgevpn(self, context, edgevpn_id, edgevpn):
+        old_edgevpn = self.get_edgevpn(context, edgevpn_id)
+        new_edgevpn = super(EdgeVPNDriverPlugin,
+                            self).update_edgevpn(context, edgevpn_id, edgevpn)
+        """Get service driver."""
+        driver = self._get_driver()
+        """Call service driver to update edgevpn instance on device."""
+        edgevpn = driver.update_edgevpn(context, old_edgevpn, new_edgevpn)
+        return new_edgevpn
+
+    def delete_edgevpn(self, context, edgevpn_id):
+        """Get EdgeVPN instances from DB by the given id."""
+        edgevpn = self._get_edgevpn(context, edgevpn_id)
+        """Get service driver."""
+        driver = self._get_driver()
+        """Call service driver to delete edgevpn instance on the device."""
+        driver.delete_edgevpn(context, edgevpn)
+        """Delete the instance from the DB."""
+        super(EdgeVPNDriverPlugin, self).delete_edgevpn(context, edgevpn_id)
+
+    def create_attachment_circuit(self, context, attachment_circuit):
+        """Call super to create row in DB."""
+        attachcircuit = (super(EdgeVPNDriverPlugin, self).
+                         create_attachment_circuit(context,
+                                                   attachment_circuit))
+        return attachcircuit
+
+    def update_attachment_circuit(self, context, attachmentcircuit_id,
+                                  attachment_circuit):
+        old_ac = self.get_attachment_circuit(context, attachmentcircuit_id)
+        new_ac = super(EdgeVPNDriverPlugin,
+                       self).update_attachment_circuit(context,
+                                                       attachmentcircuit_id,
+                                                       attachment_circuit)
+        """Get service driver."""
+        driver = self._get_driver()
+        """Call service driver to update attachment circuit on the device."""
+        attachment_circuit = driver.update_attachment_circuit(context,
+                                                              old_ac, new_ac)
+        return new_ac
+
+    def delete_attachment_circuit(self, context, attachmentcircuit_id):
+        """Get AttachmentCircuit instances from DB by the given id."""
+        self._get_attachment_circuit(context, attachmentcircuit_id)
+        """Delete the instance from the DB."""
+        super(EdgeVPNDriverPlugin,
+              self).delete_attachment_circuit(context, attachmentcircuit_id)
+
+    def create_provider_edge(self, context, provider_edge):
+        """Call super to create row in DB."""
+        provideredge = super(EdgeVPNDriverPlugin,
+                             self).create_provider_edge(context, provider_edge)
+        return provideredge
+
+    def delete_provider_edge(self, context, provideredge_id):
+        """Get Provider Edge instances from DB by the given id."""
+        self._get_provider_edge(context, provideredge_id)
+        """Delete the instance from the DB."""
+        super(EdgeVPNDriverPlugin,
+              self).delete_provider_edge(context, provideredge_id)
---
+++ networking_edge_vpn/services/edgevpn/service_drivers/brocade_edgevpn.py
@@ -0,0 +1,220 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from networking_edge_vpn.db.edgevpn import edgevpn_db
+from networking_edge_vpn.services.edgevpn import service_drivers
+
+from neutron.common import exceptions
+from neutron.i18n import _
+from neutron.plugins.common import constants
+from oslo_config import cfg
+from oslo_log import log as logging
+from oslo_utils import importutils
+
+LOG = logging.getLogger(__name__)
+
+EDGEVPN = 'edgevpn'
+MLX_DRIVER = ('neutron.services.edgevpn.device_drivers.'
+              'brocade_mlxdriver.BrocadeMLXDriver')
+VPLS_NAME = "openstack-{vpls_id}"
+mlx_opts = [cfg.StrOpt('address', default='',
+                       help='The address of the MLX to configure'),
+            cfg.StrOpt('username', default='',
+                       help='The SSH username to use'),
+            cfg.StrOpt('password', default='', secret=True,
+                       help='The SSH password to use'),
+            cfg.StrOpt('l2vpn_peer_ip', default='',
+                       help='The MCT peer loopback address'),
+            cfg.StrOpt('uplink_ports', default='',
+                       help='The uplink ports to tag'),
+            cfg.StrOpt('cluster_peer_address', default='',
+                       help='The address of the MCT cluster peer MLX'),
+            cfg.StrOpt('cluster_peer_username', default='',
+                       help='The SSH username to use'),
+            cfg.StrOpt('cluster_peer_password', default='', secret=True,
+                       help='The SSH password to use'),
+            cfg.StrOpt('cluster_peer_l2vpn_peer_ip', default='',
+                       help='The MCT peer loopback address'),
+            cfg.StrOpt('cluster_peer_uplink_ports', default='',
+                       help='The uplink ports to tag'),
+            ]
+
+cfg.CONF.register_opts(mlx_opts, "switch_mlx")
+
+
+class BrocadeEdgeVPNDriver(service_drivers.EdgeVpnDriver):
+    """Edge VPN Service Driver class for Brocade MLX."""
+
+    def __init__(self, service_plugin):
+        super(BrocadeEdgeVPNDriver, self).__init__(service_plugin)
+        self._driver = importutils.import_object(MLX_DRIVER)
+        mlx = cfg.CONF.switch_mlx
+        self._switch = {
+            'address': mlx.address,
+            'username': mlx.username,
+            'password': mlx.password,
+            'peer_ip': mlx.l2vpn_peer_ip,
+            'ports': mlx.uplink_ports,
+        }
+        self._cluster_peer_switch = None
+        if mlx.cluster_peer_address:
+            self._cluster_peer_switch = {
+                'address': mlx.cluster_peer_address,
+                'username': mlx.cluster_peer_username,
+                'password': mlx.cluster_peer_password,
+                'peer_ip': mlx.cluster_peer_l2vpn_peer_ip,
+                'ports': mlx.cluster_peer_uplink_ports,
+            }
+        self.db = edgevpn_db.EdgeVPNPluginDb()
+
+    @property
+    def service_type(self):
+        return EDGEVPN
+
+    def create_edgevpn(self, context, edgevpn):
+        """Method to create edgevpn instance on device."""
+        vpls_name = edgevpn['name']
+        vpls_id = edgevpn['vpn_id']
+        deviceType = "MLX"
+        if not vpls_name:
+            vpls_name = VPLS_NAME.format(vpls_id=vpls_id)
+            edgevpn['name'] = vpls_name
+        if not vpls_id:
+            msg = "No vpn_id specified for service, exiting"
+            LOG.error(msg)
+            raise EdgeVPNInvalidRequest()
+        with context.session.begin(subtransactions=True):
+            vlans = []
+            ac_id_list = edgevpn['attachment_circuits']
+            for ac_id in ac_id_list:
+                vlans.extend(self.db.get_vlans_for_attachment_circuit_id(
+                    context,
+                    ac_id))
+            try:
+                deviceType = self._driver.find_hardware(self._switch)
+                self._driver.configure_vpls(self._switch, vpls_id,
+                                            vpls_name, deviceType, vlans)
+                if self._cluster_peer_switch:
+                    deviceType = (self._driver.
+                                  find_hardware(self._cluster_peer_switch))
+                    self._driver.configure_vpls(self._cluster_peer_switch,
+                                                vpls_id, vpls_name,
+                                                deviceType, vlans)
+            except Exception:
+                raise EdgeVPNException(vpn_id=vpls_id)
+            edgevpn['status'] = constants.ACTIVE
+        return edgevpn
+
+    def update_edgevpn(self, context, old_edgevpn, new_edgevpn):
+        """Method to update edgevpn instance on device."""
+        old_vlans = []
+        new_vlans = []
+        with context.session.begin(subtransactions=True):
+            old_ac_id_list = old_edgevpn['attachment_circuits']
+            for ac_id in old_ac_id_list:
+                (old_vlans.extend(self.db.
+                                  get_vlans_for_attachment_circuit_id(context,
+                                                                      ac_id)))
+            new_ac_id_list = new_edgevpn['attachment_circuits']
+            for ac_id in new_ac_id_list:
+                (new_vlans.extend(self.db.
+                                  get_vlans_for_attachment_circuit_id(context,
+                                                                      ac_id)))
+            vlans_to_add = []
+            vlans_to_remove = []
+            for vlan in old_vlans:
+                if vlan not in new_vlans:
+                    vlans_to_remove.append(vlan)
+            for vlan in new_vlans:
+                if vlan not in old_vlans:
+                    vlans_to_add.append(vlan)
+            if vlans_to_add or vlans_to_remove:
+                LOG.debug("vlans_to_add: %s, vlans_to_remove: %s",
+                          vlans_to_add, vlans_to_remove)
+                try:
+                    self._driver.modify_vlans_for_vpls(self._switch,
+                                                       new_edgevpn['vpn_id'],
+                                                       new_edgevpn['name'],
+                                                       vlans_to_add,
+                                                       vlans_to_remove)
+                    if self._cluster_peer_switch:
+                        (self._driver.
+                            modify_vlans_for_vpls(self._cluster_peer_switch,
+                                                  new_edgevpn['vpn_id'],
+                                                  new_edgevpn['name'],
+                                                  vlans_to_add,
+                                                  vlans_to_remove))
+                except Exception:
+                    raise Exception(_("Brocade plugin exception, check logs"))
+        return new_edgevpn
+
+    def delete_edgevpn(self, context, edgevpn):
+        """Method to delete edgevpn instance on device."""
+        with context.session.begin(subtransactions=True):
+            vpn_id = edgevpn['vpn_id']
+            vplsname = edgevpn['name']
+            try:
+                self._driver.delete_vpls(self._switch, vplsname, vpn_id)
+                if self._cluster_peer_switch:
+                    self._driver.delete_vpls(self._cluster_peer_switch,
+                                             vplsname, vpn_id)
+            except Exception:
+                raise Exception(_("Brocade MLX plugin exception, check logs"))
+
+    def update_attachment_circuit(self, context, old_ac, new_ac):
+        """Method to update attachment circuit on device."""
+        old_vlans = []
+        new_vlans = []
+        with context.session.begin(subtransactions=True):
+            edgevpn = (self.db.
+                       get_edgevpn_for_attachment_circuit(context,
+                                                          old_ac['id']))
+            if edgevpn:
+                old_vlans = (self.db.
+                             get_vlans_for_attachment_circuit(context, old_ac))
+                new_vlans = (self.db.
+                             get_vlans_for_attachment_circuit(context, new_ac))
+                LOG.debug("old vlans %s, new vlans %s", old_vlans, new_vlans)
+                vlans_to_add = []
+                vlans_to_remove = []
+                for vlan in old_vlans:
+                    if vlan not in new_vlans:
+                        vlans_to_remove.append(vlan)
+                for vlan in new_vlans:
+                    if vlan not in old_vlans:
+                        vlans_to_add.append(vlan)
+                if vlans_to_add or vlans_to_remove:
+                    LOG.debug("vlans_to_add: %s, vlans_to_remove: %s",
+                              vlans_to_add, vlans_to_remove)
+                    try:
+                        self._driver.modify_vlans_for_vpls(self._switch,
+                                                           edgevpn['vpn_id'],
+                                                           edgevpn['name'],
+                                                           vlans_to_add,
+                                                           vlans_to_remove)
+                        if self._cluster_peer_switch:
+                            (self._driver.
+                                modify_vlans_for_vpls(
+                                    self._cluster_peer_switch,
+                                    edgevpn['vpn_id'], edgevpn['name'],
+                                    vlans_to_add, vlans_to_remove))
+                    except Exception:
+                        raise Exception(_("plugin exception, check logs"))
+        return new_ac
+
+
+class EdgeVPNInvalidRequest(exceptions.InvalidInput):
+    message = "No vpn_id specified for edgevpn service, exiting"
+
+
+class EdgeVPNException(exceptions.NeutronException):
+    message = _("Error while creating Edge VPN with ID %(vpn_id)s.")
